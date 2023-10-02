0f77b0d1a75f051560760e5a1ebe232d66523d57
MatheusCamargoGinebro <matheus.ginebro@aluno.ifsp.edu.br>
2023-10-02 09:25:07 -0300

    Typewrite done

--- src/App/Pages/LandingPage.jsx
+++ src/App/Pages/LandingPage.jsx
@@ -1,21 +1,69 @@
-import UnderlineTx from '../../Components/Basic/UnderlineTx';
+import React, { useState } from 'react';
 import Header from '../../Components/Header/Header';
+import { Typewriter } from 'react-simple-typewriter';
+import namiczImg from '../../assets/images/namicz-icon.svg';
+import UnderlineTx from '../../Components/Basic/UnderlineTx';
 
 function LandingPage() {
+  const [showNamicz, setshowNamicz] = useState(false);
+  const [showRest, setshowRest] = useState(false);
+
+  var cont = 0;
   return (
     <>
-      <Header/>
-      <div className='mb-32 flex'/>
+      <Header />
+      <div className='mb-32 flex' />
+
       <div className='text-center justify-center w-full'>
-        <h1 className='text-2xl'>Bem vindo ao <UnderlineTx text="Namicz"/>! A nossa plataforma devisualização de dados demográficos e geográficos!</h1>
+        <h1 className='text-2xl'>
+          <Typewriter
+            words={['Bem vindo ao ']}
+            loop={1}
+            cursor={false}
+            typeSpeed={70}
+            onType={
+              () => {
+                cont++;
+                if (cont === 12) {
+                  setshowNamicz(true);
+                  cont = 0;
+                }
+              }
+            }
+          />
+
+          {!!showNamicz &&
+          <span className='border-b-4 font-medium border-slate-800'>
+          <Typewriter
+            words={['Namicz!']}
+            loop={1}
+            cursor={false}
+            typeSpeed={70}
+            onType={() => {
+              cont++;
+              if (cont === 7) {
+                setshowRest(true);
+              }
+            }}
+          />
+        </span>}
+
+        {!!showRest &&
+          <Typewriter
+            words={[' ','a noss',' A nossa plataforma de visualização de dados demográficos e geográficos!']}
+            loop={1}
+            cursor={false}
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={40}
+          />
+        }
+        </h1>
       </div>
 
       
-
-      <div className='h-[125vh]'/>
     </>
   );
 }
-
 
 export default LandingPage;
--- src/Components/Basic/UnderlineTx.jsx
+++ src/Components/Basic/UnderlineTx.jsx
@@ -1,6 +1,4 @@
 const UnderlineTx = ({text}) => {
-
-
   return (
     <span className='border-b-4 font-medium border-slate-800'>{text}</span>
   );
