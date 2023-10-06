af4763cbccde029135154dbe77982063a55516e2
MatheusCamargoGinebro <matheus.ginebro@aluno.ifsp.edu.br>
2023-10-06 09:56:28 -0300

    finish map

--- src/App/Pages/MapScreen.jsx
+++ src/App/Pages/MapScreen.jsx
@@ -7,7 +7,6 @@ import { getNames } from "../../utils/getNames.js";
 
 // json:
 import geojson from "../../json/geojson.json";
-import state from "../../json/state.json";
 
 // Components:
 import Map, { Source, Layer } from "react-map-gl";
@@ -19,8 +18,7 @@ import FilterAltIcon from "@mui/icons-material/FilterAlt";
 
 function MapScreen() {
   const [hoverInfo, setHoverInfo] = useState(null);
-  
-  const [openNavBar, setOpenNavBar] = useState(false);
+  const [haveInfo, setHaveInfo] = useState(false);
 
   const onHover = useCallback((event) => {
     const county = event.features[0] ? event.features[0].properties.estado : "";
@@ -28,6 +26,12 @@ function MapScreen() {
       ? event.features[0].properties.codarea
       : "";
     const acronym = event.features[0] ? event.features[0].properties.sigla : "";
+
+    if (!!county != "") {
+      setHaveInfo(true);
+    } else {
+      setHaveInfo(false);
+    }
 
     setHoverInfo({
       county: county,
@@ -75,13 +79,12 @@ function MapScreen() {
 
   const mapRef = useRef();
   const GEOFENCE = turf.circle([-52.4, -16.3], 3000, { units: "kilometers" });
-  
+
   //impedindo que o brasil saia da tela e mantendo uma boa proporção/margem conforme o zoom:
   const bounds = [
     [-80, 15], // Southwest coordinates
-    [-30, -15] // Northeast coordinates
-  ];  
-
+    [-30, -15], // Northeast coordinates
+  ];
 
   const onSelectCity = useCallback(({ longitude, latitude }, duration) => {
     mapRef.current?.flyTo({
@@ -100,50 +103,64 @@ function MapScreen() {
   return (
     <>
       <Drawerbar />
-      <div className='w-full h-screen bg-slate-300 overflow-hidden'>
-      <Map
-        // Estilos:
+      <div className="w-full h-screen bg-slate-300 overflow-hidden">
+        <Map
+          // Estilos:
           mapStyle="mapbox://styles/camarg0vs/clm1c13c401ub01p7g8sngg8x"
           mapboxAccessToken="pk.eyJ1IjoiY2FtYXJnMHZzIiwiYSI6ImNsbGtyeHkwNzIzYXYzcW8xYTk4dXplOTkifQ.AeclKAsX4UhZf6xCfZgwPg"
           style={{ width: "100%", height: "100vh" }}
-        
-        // Dados:
+          // Dados:
           {...viewState}
           interactiveLayerIds={["data"]}
           ref={mapRef}
-
-        // Definições:
-          zoom={1}
+          // Definições:
+          zoom={2.5}
           doubleClickZoom={false}
           maxPitch={45}
           minPitch={0}
           scrollZoom={false}
-
+          cursor={haveInfo ? "pointer" : "default"}
           maxBounds={bounds}
-        
-        // Eventos:
+          // Eventos:
           onMouseMove={onHover}
           onMove={onMove}
           onClick={() => {
-            if(!!hoverInfo.county!=''){  
-              onSelectCity({longitude: hoverInfo.longitude, latitude: hoverInfo.latitude}, 1000);
+            if (!!hoverInfo.county != "") {
+              onSelectCity(
+                {
+                  longitude: hoverInfo.longitude,
+                  latitude: hoverInfo.latitude,
+                },
+                1000
+              );
               getNames(hoverInfo.codearea).then((res) => {
                 setClickedCounty(selectedCounty);
                 setClickedCodeArea(hoverInfo.codearea);
-                setRanking(res[0].res)
+                setRanking(res[0].res);
                 setModalOpen(true);
-              })
+              });
             } else {
-              if (!turf.booleanPointInPolygon([hoverInfo.longitude, hoverInfo.latitude, hoverInfo.zoom], GEOFENCE)) {
-                onSelectCity({longitude: -47, latitude: -15}, 1000);
+              if (
+                !turf.booleanPointInPolygon(
+                  [hoverInfo.longitude, hoverInfo.latitude, hoverInfo.zoom],
+                  GEOFENCE
+                )
+              ) {
+                onSelectCity({ longitude: -47, latitude: -15 }, 1000);
               }
             }
-          }}
-        >
+          }}>
           <Source type="geojson" data={geojson}>
             <Layer {...layerStyle} />
           </Source>
         </Map>
+        <Modal
+          open={modalOpen}
+          handleClose={() => setModalOpen(false)}
+          countyName={clickedCounty}
+          codeArea={clickedCodeArea}
+          ranking={ranking}
+        />
       </div>
     </>
   );
