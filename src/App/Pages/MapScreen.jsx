1bb109573d6f5a348bd8e14dbe7ea2d5adceaba7
MatheusCamargoGinebro <matheus.ginebro@aluno.ifsp.edu.br>
2023-10-06 09:42:36 -0300

    MapDone

--- src/App/Pages/MapScreen.jsx
+++ src/App/Pages/MapScreen.jsx
@@ -18,46 +18,42 @@ import Drawerbar from "../../Components/Drawerbar/Drawerbar.jsx";
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
 
 function MapScreen() {
-  // Estado de componentes e dados:
   const [hoverInfo, setHoverInfo] = useState(null);
-  const [viewState, setViewState] = useState({ longitude: -53.2, latitude: -10.9 });
-  const [modalOpen, setModalOpen] = useState(false);
-  const [clickedCounty, setClickedCounty] = useState(false);
-  const [clickedCodeArea, setClickedCodeArea] = useState(false);
-  const [ranking, setRanking] = useState(null);
+  
+  const [openNavBar, setOpenNavBar] = useState(false);
 
-  const selectedCounty = (hoverInfo && hoverInfo.county) || "";
-  const GEOFENCE = turf.circle([-52.4, -16.3], 3000, { units: "kilometers" });
-
-  // funções:
-  // Atualizar quando mexer:
   const onHover = useCallback((event) => {
-    // tentando coletar dados em malhas:
     const county = event.features[0] ? event.features[0].properties.estado : "";
-    const codeArea = event.feature[0]
-      ? event.features[0].properties.codearea
+    const codearea = event.features[0]
+      ? event.features[0].properties.codarea
       : "";
-    const acronym = event.feature[0] ? event.feature.properties.sigla : "";
+    const acronym = event.features[0] ? event.features[0].properties.sigla : "";
 
-    // definindo informações de hover:
     setHoverInfo({
       county: county,
-      codearea: codeArea,
+      codearea: codearea,
       acronym: acronym,
       longitude: event.lngLat.lng,
       latitude: event.lngLat.lat,
     });
   }, []);
 
-  // pegar dados ao clicar:
-  const flyToPlace = useCallback(({ longitude, latitude }, duration) => {
-    mapRef.current?.flyTo({
-      center: [longitude, latitude],
-      duration: duration,
+  const [viewState, setViewState] = useState({
+    longitude: -47,
+    latitude: -15,
+    zoom: 3.8,
+  });
+
+  const onMove = useCallback(({ viewState }) => {
+    setViewState({
+      longitude: viewState.longitude,
+      latitude: viewState.latitude,
+      zoom: viewState.zoom,
     });
   }, []);
 
-  // Definindo estilos de camada:
+  const selectedCounty = (hoverInfo && hoverInfo.county) || "";
+
   const layerStyle = {
     id: "data",
     type: "fill",
@@ -65,29 +61,90 @@ function MapScreen() {
       "fill-outline-color": [
         "case",
         ["==", ["get", "estado"], selectedCounty],
-        "#0f172a",
-        "#2F3F58",
+        "#64748b",
+        "#334155",
       ],
       "fill-color": [
         "case",
         ["==", ["get", "estado"], selectedCounty ? selectedCounty : null],
-        "#0f172a",
-        "#1E293B",
-      ],
-      "fill-opacity": [
-        "case",
-        ["==", ["get", "estado"], selectedCounty ? selectedCounty : null],
-        0.8,
-        0.7,
+        "#475569",
+        "#334155",
       ],
     },
   };
 
   const mapRef = useRef();
+  const GEOFENCE = turf.circle([-52.4, -16.3], 3000, { units: "kilometers" });
+  
+  //impedindo que o brasil saia da tela e mantendo uma boa proporção/margem conforme o zoom:
+  const bounds = [
+    [-80, 15], // Southwest coordinates
+    [-30, -15] // Northeast coordinates
+  ];  
+
+
+  const onSelectCity = useCallback(({ longitude, latitude }, duration) => {
+    mapRef.current?.flyTo({
+      center: [longitude, latitude],
+      duration: duration,
+    });
+  }, []);
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  //HARCODE
+  const [clickedCounty, setClickedCounty] = useState(false);
+  const [clickedCodeArea, setClickedCodeArea] = useState(false);
+  const [ranking, setRanking] = useState(null);
 
   return (
     <>
       <Drawerbar />
+      <div className='w-full h-screen bg-slate-300 overflow-hidden'>
+      <Map
+        // Estilos:
+          mapStyle="mapbox://styles/camarg0vs/clm1c13c401ub01p7g8sngg8x"
+          mapboxAccessToken="pk.eyJ1IjoiY2FtYXJnMHZzIiwiYSI6ImNsbGtyeHkwNzIzYXYzcW8xYTk4dXplOTkifQ.AeclKAsX4UhZf6xCfZgwPg"
+          style={{ width: "100%", height: "100vh" }}
+        
+        // Dados:
+          {...viewState}
+          interactiveLayerIds={["data"]}
+          ref={mapRef}
+
+        // Definições:
+          zoom={1}
+          doubleClickZoom={false}
+          maxPitch={45}
+          minPitch={0}
+          scrollZoom={false}
+
+          maxBounds={bounds}
+        
+        // Eventos:
+          onMouseMove={onHover}
+          onMove={onMove}
+          onClick={() => {
+            if(!!hoverInfo.county!=''){  
+              onSelectCity({longitude: hoverInfo.longitude, latitude: hoverInfo.latitude}, 1000);
+              getNames(hoverInfo.codearea).then((res) => {
+                setClickedCounty(selectedCounty);
+                setClickedCodeArea(hoverInfo.codearea);
+                setRanking(res[0].res)
+                setModalOpen(true);
+              })
+            } else {
+              if (!turf.booleanPointInPolygon([hoverInfo.longitude, hoverInfo.latitude, hoverInfo.zoom], GEOFENCE)) {
+                onSelectCity({longitude: -47, latitude: -15}, 1000);
+              }
+            }
+          }}
+        >
+          <Source type="geojson" data={geojson}>
+            <Layer {...layerStyle} />
+          </Source>
+        </Map>
+      </div>
     </>
   );
 }
